b37152d45ed0af5b66e96796d3904feb3cf46554
Michael <ultimate2424@gmail.com>
2016-12-13 11:28:05 +0800

    First commit

---
+++ app/src/main/java/com/example/michael/testdeeplink/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.michael.testdeeplink;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by michael on 16-12-13.
+ */
+
+public class DetailActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
